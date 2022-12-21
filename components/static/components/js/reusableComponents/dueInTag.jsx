2d1fba9fde9847bf9c8be31a2ebdc17fb67b2cbd
Emma Faurie <emma.iris.faurie@gmail.com>
2022-12-21 13:15:46 +0100

    Update Colors

--- components/static/components/js/reusableComponents/dueInTag.jsx
+++ components/static/components/js/reusableComponents/dueInTag.jsx
@@ -1,11 +1,8 @@
 import { h } from "preact";
 import AlarmIcon from "@mui/icons-material/Alarm";
-import saltise from "../theme";
 import { Tag } from "../styledComponents";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-
-const theme = saltise;
 
 const daysDiff = (date) => {
   const currentDate = new Date();
@@ -24,8 +21,8 @@ export const DueInTag = (props) => {
         <Tag
           sx={{
             mr: "10px",
-            color: theme.palette.red.main,
-            backgroundColor: theme.palette.paleRed.main,
+            color: "red.main",
+            backgroundColor: "paleRed.main",
           }}
         >
           <AlarmIcon fontSize="small" sx={{ pr: "6px" }} />

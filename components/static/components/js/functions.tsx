e29a9331207221e225ce7fef52bd82efddcfd393
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2023-07-09 19:46:31 -0400

    Fix due date calc bug

--- components/static/components/js/functions.tsx
+++ components/static/components/js/functions.tsx
@@ -14,7 +14,7 @@ export const daysDiff = (dueDate: Date) => {
     // (YYYY-MM-DD)
     const millisBetween: number = dueDate.getTime() - currentDate.getTime();
     const days: number = millisBetween / (1000 * 3600 * 24);
-    return Math.round(Math.abs(days));
+    return Math.round(days);
   }
   return undefined;
 };

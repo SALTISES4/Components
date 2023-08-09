dd1bcf3935bc9a286833c332c13f5bba59cf676a
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2023-08-08 22:54:33 -0400

    Full rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -32,13 +32,13 @@ const tsProject = ts.createProject("tsconfig.json");
 // ];
 
 const modules = [
-  // "dashboard",
-  // "navigation",
-  // "createAssignment",
-  // "createQuestions1",
-  // "updateAssignment",
+  "dashboard",
+  "navigation",
+  "createAssignment",
+  "createQuestions1",
+  "updateAssignment",
   "library",
-  // "search",
+  "search",
   // "styleguide",
 ];
 
